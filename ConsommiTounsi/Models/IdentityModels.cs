2cc1784ce09d9fd1387188e5d3556e492d7a835f
mahdi chargui <mahdi.chargui@esprit.tn>
2020-07-23 22:22:34 +0100

    Ad management

--- ConsommiTounsi/Models/IdentityModels.cs
+++ ConsommiTounsi/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace ConsommiTounsi.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<ConsommiTounsi.Models.Ad> Ads { get; set; }
     }
 }
--- ConsommiTounsi/Models/Supplier.cs
+++ ConsommiTounsi/Models/Supplier.cs
@@ -10,5 +10,27 @@ namespace ConsommiTounsi.Models
     {
         [JsonProperty("userId")]
         public long userId { get; set; }
+        [JsonProperty("userName")]
+        public string userName { get; set; }
+        [JsonProperty("password")]
+        public string password { get; set; }
+        [JsonProperty("role")]
+        public string role { get; set; }
+        [JsonProperty("firstName")]
+        public string firstName { get; set; }
+        [JsonProperty("lastName")]
+        public string lastName { get; set; }
+        [JsonProperty("birthdate")]
+        public string birthday { get; set; }
+        [JsonProperty("mail")]
+        public string mail { get; set; }
+        [JsonProperty("phone")]
+        public string phone { get; set; }
+        [JsonProperty("urlImage")]
+        public string urlImage { get; set; }
+        [JsonProperty("gender")]
+        public string gender { get; set; }
+        [JsonProperty("address")]
+        public string address { get; set; }
     }
 }
