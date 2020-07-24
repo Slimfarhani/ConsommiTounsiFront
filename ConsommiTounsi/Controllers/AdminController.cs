d406bb998431ec0c45caa507a200dea0935c5a99
louaihaj <louaihaj@gmail.com>
2020-07-24 04:22:28 +0200

    Customer management

--- ConsommiTounsi/Controllers/AdminController.cs
+++ ConsommiTounsi/Controllers/AdminController.cs
@@ -2,9 +2,11 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -19,6 +21,72 @@ namespace ConsommiTounsi.Controllers
         {
             return View();
         }
+
+        public ActionResult CustomerDetails( long id)
+        {
+            System.Diagnostics.Debug.WriteLine("heloooooooo !!!!");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8080/springboot-crud-rest/api/v1/customer/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response;
+            response = client.GetAsync("search/" + id).Result;
+            Customer customer= response.Content.ReadAsAsync<Customer>().Result;
+            System.Diagnostics.Debug.WriteLine(customer.lastName);
+            return View(customer);
+           
+        }
+        public ActionResult UpdateCustomer( int id)
+        {
+
+                System.Diagnostics.Debug.WriteLine("heloooooooo !!!!");
+                HttpClient client1 = new HttpClient();
+                client1.BaseAddress = new Uri("http://localhost:8080/springboot-crud-rest/api/v1/customer/");
+                client1.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response;
+                response = client1.GetAsync("search/" + id).Result;
+                Customer customer = response.Content.ReadAsAsync<Customer>().Result;
+                System.Diagnostics.Debug.WriteLine(customer.lastName);
+            customer.birthdateString = customer.birthdateFormatted.ToString();
+            return View(customer);
+            
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateCustomer(Customer  model, HttpPostedFileBase file )
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                if(file != null)
+                { 
+                    var newFileName = file.FileName + model.userName + Path.GetExtension(file.FileName);
+                    model.urlImage = newFileName;
+                    var path = Path.Combine(Server.MapPath("~/UserImages/"), newFileName);
+                    file.SaveAs(path);
+                }
+              
+              
+                model.birthdateFormatted = Convert.ToDateTime(model.birthdateString);
+                var customerJson = await Task.Run(() => JsonConvert.SerializeObject(model));
+                System.Diagnostics.Debug.WriteLine(customerJson.ToString());
+                
+               
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:8080/springboot-crud-rest/api/v1/");
+
+                var content = new StringContent(customerJson.ToString(), Encoding.UTF8, "application/json");
+                System.Diagnostics.Debug.WriteLine(content.ReadAsStringAsync());
+                HttpResponseMessage response = client.PutAsync("customer/update/"+model.userId, content).Result;
+
+                System.Diagnostics.Debug.WriteLine(response);
+                return RedirectToAction("/customermanagement");
+
+            }
+           
+            return View(model);
+        }
         public ActionResult CustomerManagement()
         {
 
@@ -31,6 +99,21 @@ namespace ConsommiTounsi.Controllers
             return View(Customers);
 
         }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:8080/springboot-crud-rest/api/v1/customer/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.DeleteAsync("delete/"+id).Result;
+                return RedirectToAction("CustomerManagement");
+            }
+            catch
+            {
+                return View();
+            }
+        }
         // GET: /Account/Register
         [AllowAnonymous]
         public ActionResult AddCustomer()
@@ -41,14 +124,19 @@ namespace ConsommiTounsi.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> AddCustomer(Customer model)
+        public async Task<ActionResult> AddCustomer(Customer model , HttpPostedFileBase file)
         {
             
             if (ModelState.IsValid)
             {
+
+                var newFileName = file.FileName+model.userName + Path.GetExtension(file.FileName);
+                model.urlImage = newFileName;
+                //model.birthdateFormatted = Convert.ToDateTime(model.birthdateString);
                 var customerJson = await Task.Run(() => JsonConvert.SerializeObject(model));
                 System.Diagnostics.Debug.WriteLine(customerJson.ToString());
-
+                var path = Path.Combine(Server.MapPath("~/UserImages/"), newFileName);
+                file.SaveAs(path);
                 HttpClient client = new HttpClient();
                 client.BaseAddress = new Uri("http://localhost:8080/springboot-crud-rest/api/v1/");
 
@@ -62,5 +150,7 @@ namespace ConsommiTounsi.Controllers
             }
             return View(model);
         }
+      
     }
+
 }
--- ConsommiTounsi/Models/Customer.cs
+++ ConsommiTounsi/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,27 +11,46 @@ namespace ConsommiTounsi.Models
     {
         [JsonProperty("userId")]
         public long userId { get; set; }
+       
         [JsonProperty("userName")]
+        [Required]
         public string userName { get; set; }
+        
         [JsonProperty("password")]
+        [DataType(DataType.Password)]
+        [Required]
         public string password { get; set; }
+        
         [JsonProperty("role")]
         public string role { get; set; }
+        [Required]
         [JsonProperty("firstName")]
         public string firstName { get; set; }
+        [Required]
         [JsonProperty("lastName")]
         public string lastName { get; set; }
+
+        public string birthdateString { get; set; }
+        [Required(ErrorMessage = "birthdate. is required")]
         [JsonProperty("birthdate")]
-        public string birthday { get; set; }
+        public DateTime birthdateFormatted { get; set; }
         [JsonProperty("mail")]
-        public string mail { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Mobile no. is required")]
+        [RegularExpression("((\\+|00)216)?(7|2|5|9|4)[0-9]{7}", ErrorMessage = "Please enter valid phone no.")]
+
         [JsonProperty("phone")]
         public string phone { get; set; }
-        [JsonProperty("urlImage")]
-        public string urlImage { get; set; }
-        [JsonProperty("gender")]
-        public string gender { get; set; }
         [JsonProperty("address")]
+
         public string address { get; set; }
+        [JsonProperty("gender")]
+        [Required]
+        public string gender { get; set; }
+
+        [JsonProperty("urlImage")]
+        
+        public string urlImage { get; set; }
     }
 }
